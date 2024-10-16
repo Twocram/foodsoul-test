00323a904bd339009d454220b4a37563a5a71c47
Artem Maslov <2002maslovartem@gmail.com>
2024-10-17 00:01:21 +0500

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <RouterView />
+</template>
+
+<style>
+@font-face {
+  font-family: "Inter";
+  font-style: italic;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/inter/v12/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7SUc.woff2)
+    format("woff2");
+}
+.inter {
+  font-family: "Inter";
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/entry-server.ts
@@ -0,0 +1,13 @@
+import { renderToString } from 'vue/server-renderer';
+import { createApp } from './main';
+
+export async function render(url: string) {
+  const { app, router } = createApp();
+
+  router.push(url);
+  await router.isReady();
+
+  const ctx = {};
+  const html = await renderToString(app, ctx);
+  return { html };
+}
