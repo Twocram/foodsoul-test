064dbda9904a1c051aebab2517ff5f9a85d9f4a7
Artem Maslov <2002maslovartem@gmail.com>
2024-10-17 11:14:19 +0500

    chore: rename method into locationService.ts

--- src/services/locationService.ts
+++ src/services/locationService.ts
@@ -1,12 +1,12 @@
 import type { NominatimLocation } from "../types/nominatim";
 
-export async function getLocation(query: string): Promise<{ data: NominatimLocation[], error: Error }> {
+export async function getLocations(query: string): Promise<{ data: NominatimLocation[], error: unknown }> {
     try {
         const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(query)}&format=json`);
         const data = await response.json();
         
         return { data, error: null };
-    } catch (err: Error) {
+    } catch (err: unknown) {
         return { data: [], error: err };
     }
 }
